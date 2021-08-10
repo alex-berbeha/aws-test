0c40c567369a38e62adfd75bd0b5b01a7aa53bc5
berbeha.olexandr <berbeha.olexandr@gmail.com>
2021-08-10 16:26:48 +0300

    add sg and eip

--- network.tf
+++ network.tf
@@ -22,19 +22,34 @@ module "public-subnet" {
 resource "aws_route_table" "public_RT" {
   vpc_id = module.main-vpc.VPC_id
 
-  route = [
-    {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = module.main-vpc.igw_id
-    }
-  ]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = module.main-vpc.igw_id
+  }
 
   tags = {
-    Name = "pivate-subnet-RT"
+    Name = "public-subnet-RT"
   }
 }
 
 resource "aws_route_table_association" "public_subnet" {
   route_table_id = aws_route_table.public_RT.id
   subnet_id      = module.public-subnet.subnet_id
+}
+
+module "security_groups" {
+  source               = "github.com/alex-berbeha/terraform-modules.git/AWS/modules/security_groups"
+  sg_name              = "allow ssh and http ingress"
+  vpc_id               = module.main-vpc.VPC_id
+  ingress_allows_ports = [22, 80]
+  protocol             = "tcp"
+  cidr_list            = ["0.0.0.0/0"]
+  sg_tags = {
+    Name = "allow ssh and http ingress"
+  }
+
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
 }
